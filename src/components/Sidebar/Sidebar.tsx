8ca987401ba5eb875435a563a0d644cc3d9fe7bc
Mauro Sanchez <maurosanchez.work@gmail.com>
2024-09-01 16:04:18 +0900

    Made Sidebar bg transparent

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -25,8 +25,8 @@ const Sidebar: React.FC = () => {
             </button>
 
             <div
-                className={`fixed lg:left-0 lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:w-64 lg:h-auto w-full h-full top-0 left-0 bg-white z-40 transition-transform ${
-                    isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
+                className={`fixed lg:left-0 lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:w-64 lg:h-auto w-full h-full top-0 left-0 z-40 transition-transform ${
+                    isOpen ? 'translate-x-0 bg-white ' : '-translate-x-full lg:translate-x-0'
                 }`}
             >
                 <div className="flex flex-col p-4 space-y-4">
