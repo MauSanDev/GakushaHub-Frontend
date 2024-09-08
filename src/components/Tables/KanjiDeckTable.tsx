1d9244c3d9369326a45c2e6b0a1eafcf9e520885
Mauro Sanchez <maurosanchez.work@gmail.com>
2024-09-08 12:04:11 +0900

    Fixed table's format

--- src/components/Tables/KanjiDeckTable.tsx
+++ src/components/Tables/KanjiDeckTable.tsx
@@ -12,10 +12,10 @@ const KanjiDeckTable: React.FC<KanjiDeckTableProps> = ({ deck }) => {
                 <table className=" overflow-scroll w-full bg-white dark:bg-black text-xs dark:border dark:border-gray-800">
                     <thead>
                     <tr className="bg-blue-50 dark:bg-gray-950 text-center text-sm dark:text-gray-300">
-                        <th className="px-4 py-2 font-bold whitespace-nowrap">Kanji</th>
-                        <th className="px-4 py-2 ">Onyomi</th>
-                        <th className="px-4 py-2 ">Kunyomi</th>
-                        <th className="px-4 py-2 ">Meanings</th>
+                        <th className="px-4 py-2 font-bold whitespace-nowrap">漢字</th>
+                        <th className="px-4 py-2 ">音読み</th>
+                        <th className="px-4 py-2 ">訓読み</th>
+                        <th className="px-4 py-2 ">意味</th>
                         <th className="px-4 py-2 ">JLPT</th>
                     </tr>
                     </thead>
@@ -27,8 +27,8 @@ const KanjiDeckTable: React.FC<KanjiDeckTableProps> = ({ deck }) => {
                                 index % 2 === 0 ? "bg-gray-50 dark:bg-gray-900" : "bg-white dark:bg-gray-800"
                             } hover:bg-blue-50 transition duration-200 text-gray-800 dark:text-gray-200 text-left`}
                         >
-                            <td className="px-4 py-2 font-bold ">{element.kanji}</td>
-                            <td className="px-4 py-2 ">{element.readings.onyomi.join("; ")}</td>
+                            <td className="px-4 py-2 font-bold">{element.kanji}</td>
+                            <td className="px-4 py-2 break-words whitespace-nowrap">{element.readings.onyomi.join("; ")}</td>
                             <td className="px-4 py-2 ">{element.readings.kunyomi.join("; ")}</td>
                             <td className="px-4 py-2 ">
                                 {element.meanings?.map((meaning) => meaning.en).join("; ") || ""}
--- src/components/Tables/WordDeckTable.tsx
+++ src/components/Tables/WordDeckTable.tsx
@@ -8,15 +8,14 @@ interface WordDeckTableProps {
 const WordDeckTable: React.FC<WordDeckTableProps> = ({ deck }) => {
     return (
         <div className="overflow-x-auto mb-5 max-w-full">
-            <div className="inline-block align-middle overflow-scroll max-w-[450px] lg:max-w-full">
+            <div className="inline-block align-middle overflow-scroll lg:max-w-full">
                 <table className=" overflow-scroll w-full bg-white dark:bg-black text-xs dark:border dark:border-gray-800">
                     <thead>
                     <tr className="bg-blue-50 dark:bg-gray-950 text-center text-sm dark:text-gray-300">
-                        <th className="px-4 py-2 font-bold">Word</th>
-                        <th className="px-4 py-2">Readings</th>
-                        <th className="px-4 py-2">Meanings</th>
+                        <th className="px-4 py-2 font-bold">言葉</th>
+                        <th className="px-4 py-2">読み方</th>
+                        <th className="px-4 py-2">意味</th>
                         <th className="px-4 py-2">Part of Speech</th>
-                        <th className="px-4 py-2">Related Words</th>
                     </tr>
                     </thead>
                     <tbody className="text-center text-sm">
@@ -27,13 +26,10 @@ const WordDeckTable: React.FC<WordDeckTableProps> = ({ deck }) => {
                                 index % 2 === 0 ? "bg-gray-50 dark:bg-gray-900" : "bg-white dark:bg-gray-800"
                             } hover:bg-blue-50 transition duration-200 text-gray-800 dark:text-gray-200 text-left`}
                         >
-                            <td className="px-4 py-2 font-bold">{element.word}</td>
-                            <td className="px-4 py-2">{element.readings.join("; ")}</td>
-                            <td className="px-4 py-2">{element.meanings?.map((meaning) => meaning.en).join("; ") || ""}</td>
-                            <td className="px-4 py-2">{element.part_of_speech.join("; ")}</td>
-                            <td className="px-4 py-2">
-                                {element.related_words.map((related) => related.text).join(", ")}
-                            </td>
+                            <td className="px-4 py-2 font-bold break-words whitespace-nowrap">{element.word}</td>
+                            <td className="px-4 py-2 break-words whitespace-nowrap">{element.readings.join("; ")}</td>
+                            <td className="px-4 py-2 max-w-xs whitespace-normal break-words">{element.meanings?.map((meaning) => meaning.en).join("; ") || ""}</td>
+                            <td className="px-4 py-2 max-w-32 truncate">{element.part_of_speech.join("; ")}</td>
                         </tr>
                     ))}
                     </tbody>
