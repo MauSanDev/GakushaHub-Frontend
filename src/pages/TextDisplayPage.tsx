d697dde12bcd3f524ce59b471a5d3f4ce2e657fa
Mauro Sanchez <maurosanchez.work@gmail.com>
2024-10-15 19:08:43 +0900

    Migrated endpoint for TextDisplay

--- src/pages/TextDisplayPage.tsx
+++ src/pages/TextDisplayPage.tsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import TextReaderElement from '../components/TextReader';
-import { useFetchElementById } from '../hooks/useFetchElement.ts';
 import { GeneratedData } from "../data/GenerationData.ts";
 import SaveDeckInput from '../components/SaveDeckInput';
-import {useAuth} from "../context/AuthContext.tsx";
+import { useAuth } from "../context/AuthContext.tsx";
 import SectionContainer from "../components/ui/containers/SectionContainer.tsx";
 import BackButton from "../components/ui/buttons/BackButton.tsx";
+import { useElements } from '../hooks/newHooks/useElements';  // Importa el nuevo hook
+import { CollectionTypes } from '../data/CollectionTypes.tsx';
 
 const TextDisplayPage: React.FC = () => {
     const { elementId } = useParams<{ elementId: string }>();
-    const { data, isLoading, error } = useFetchElementById<GeneratedData>({ id: elementId || '', elementType: 'generation' });
     const navigate = useNavigate();
-    const { isAuthenticated } = useAuth()
+    const { isAuthenticated } = useAuth();
+
+    const { data, isLoading, fetchElementsData } = useElements<GeneratedData>(
+        elementId ? [elementId] : [],
+        CollectionTypes.Generation
+    );
+
+    useEffect(() => {
+        if (elementId) {
+            fetchElementsData();
+        }
+    }, [elementId]);
+
+    const elementData = data ? data[elementId || ''] : undefined;
 
     return (
-        <SectionContainer isLoading={isLoading} error={error && String(error) || ""}>
+        <SectionContainer isLoading={isLoading} error={elementData ? '' : 'No content available'}>
             <div className="flex-1 rounded-md overflow-y-auto relative max-w-4xl w-full">
-                {data ? (
+                {elementData ? (
                     <>
+                        {isAuthenticated && (
+                            <div className="absolute top-0 right-0 flex gap-2">
+                                <SaveDeckInput
+                                    kanjiList={[]}
+                                    wordList={[]}
+                                    grammarList={[]}
+                                    readingList={[elementData._id]}
+                                />
+                            </div>
+                        )}
 
-                    {isAuthenticated && (<div className="absolute top-0 right-0 flex gap-2">
-                            <SaveDeckInput
-                                kanjiList={[]} 
-                                wordList={[]}
-                                grammarList={[]}
-                                readingList={[data._id]} />
-                        </div>)}
-                        
                         <div className="flex items-center justify-between mb-4 lg:pl-0 pl-20 mt-4">
                             <BackButton onClick={() => {navigate(-1)}} />
                         </div>
 
                         <div className="relative">
-                            <TextReaderElement data={data} />
+                            <TextReaderElement data={elementData} />
                         </div>
-                        
+
                     </>
                 ) : (
                     <div className="flex items-center justify-center h-full mt-2">
                         <h1 className="text-center text-4xl text-gray-300 font-bold align-middle space-x-0">
-                            {error ? error.message : 'No content available'}
+                            {isLoading ? "Loading..." : "No content available"}
                         </h1>
                     </div>
                 )}
